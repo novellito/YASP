0e1eeca47477e20fbb8d04f1d1f212e5e2fcdc20
novellito <novellito7@gmail.com>
2019-04-15 22:36:52 -0700

    add helmetjs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const helmet = require('helmet');
 const app = express();
 
 // import routes
@@ -16,6 +17,7 @@ mongoose.connect(
 );
 
 require('./auth/localAuth');
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
