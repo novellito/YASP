4d0d5a8552a521e56c3d97c6bc909a618835ba5b
novellito <novellito7@gmail.com>
2019-05-05 22:59:04 -0700

    cleanup client code

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-/* eslint-disable no-console */
 const express = require('express');
 const next = require('next');
+
+const PORT = 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+
+const handle = app.getRequestHandler();
 
 const devProxy = {
   '/api': {
@@ -10,38 +15,22 @@ const devProxy = {
   }
 };
 
-const port = parseInt(process.env.PORT, 10) || 3000;
-const env = process.env.NODE_ENV;
-const dev = env !== 'production';
-const app = next({
-  dir: '.', // base directory where everything is, could move to src later
-  dev
-});
-
-const handle = app.getRequestHandler();
-
-let server;
 app
   .prepare()
   .then(() => {
-    server = express();
+    const server = express();
 
-    // Set up the proxy.
     if (dev && devProxy) {
       const proxyMiddleware = require('http-proxy-middleware');
-      Object.keys(devProxy).forEach(function(context) {
-        server.use(proxyMiddleware(context, devProxy[context]));
-      });
+      server.use(proxyMiddleware('/api', devProxy['/api']));
     }
 
     // Default catch-all handler to allow Next.js to handle all other routes
     server.all('*', (req, res) => handle(req, res));
 
-    server.listen(port, err => {
-      if (err) {
-        throw err;
-      }
-      console.log(`> Ready on port ${port}`);
+    server.listen(PORT, err => {
+      if (err) throw err;
+      console.log(`> Ready on port ${PORT}`);
     });
   })
   .catch(err => {
